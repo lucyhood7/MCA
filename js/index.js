499370c9b91d896372b7bb0697aca4d78e8a69b5
lucyhood7 <43408512+lucyhood7@users.noreply.github.com>
2018-12-03 15:28:43 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -20,4 +20,4 @@ options = {
 vrvToolkit.setOptions(options)
 
 /* Show the score */
-showScore('score', 'The_Office_Theme.mei') // <-- CHANGE THIS TO YOUR FILE
+showScore('score', 'The Office Theme.mei') // <-- CHANGE THIS TO YOUR FILE
